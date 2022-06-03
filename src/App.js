85604ab82bad4bac8f0285e758df674ddad6c45e
Samantha Wu <samanthayywu@gmail.com>
2022-06-03 18:05:08 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Navbar";
 
+// root components- first thing to render
 function App() {
   return (
     // the div that's been injected to html
     <div className="App">
+      {/* inject navbar into root route */}
+      <Navbar />
       <div className="content">
         <h1>Accidentally Vegan</h1>
       </div>
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Accidentally Vegan</h1>
+      <a href="/">Home</a>
+      <a href="/create">Create</a>
+    </div>
+  );
+};
+
+export default Navbar;
