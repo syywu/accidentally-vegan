e50adcbf586dcc01bb9cab78139c2d502c6a7ecf
Samantha Wu <samanthayywu@gmail.com>
2022-06-30 15:14:54 +0100

    root query

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -10,3 +10,16 @@ const ProductType = new GraphQLObjectType({
     type: { type: GraphQLString },
   }),
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    product: {
+      type: ProductType,
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        // code to get data from db/other sources
+      },
+    },
+  },
+});
