7938c5f882063dad9492b94b224f678f908f3b70
Samantha Wu <samanthayywu@gmail.com>
2022-06-30 13:32:47 +0100

    product type

--- backend/schema/schema.js
+++ backend/schema/schema.js
@@ -1,1 +1,12 @@
 import graphql from "graphql";
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const ProductType = new GraphQLObjectType({
+  name: "Product",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    type: { type: GraphQLString },
+  }),
+});
