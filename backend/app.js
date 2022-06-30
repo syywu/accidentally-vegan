d56e817ed20dd2c2743913f9311e49875054e6a6
Samantha Wu <samanthayywu@gmail.com>
2022-06-30 11:02:17 +0100

    schema

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
 const PORT = 8000;
+app.use("/graphql", graphqlHTTP());
 
 app.listen(PORT, () => {
   console.log(`now listening on port ${PORT}`);
