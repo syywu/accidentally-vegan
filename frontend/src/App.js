d84b5950876de6c9698fdfbc5537f08b5b58c977
Samantha Wu <samanthayywu@gmail.com>
2022-07-03 20:49:42 +0100

    apollo client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,31 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 // root components- first thing to render
 function App() {
+  const client = new ApolloClient({
+    uri: "https://http://localhost:8000/graphql/",
+    cache: new InMemoryCache(),
+  });
   return (
-    // the div that's been injected to html
-    <div className="App">
-      {/* inject navbar into root route */}
-      <Navbar />
-      <div className="content">
-        <h1>Accidentally Vegan</h1>
+    <ApolloProvider client={client}>
+      // the div that's been injected to html
+      <div className="App">
+        {/* inject navbar into root route */}
+        <Navbar />
+        <ProductList />
+        <div className="content">
+          <h1>Accidentally Vegan</h1>
+        </div>
       </div>
-    </div>
+    </ApolloProvider>
   );
 }
 
